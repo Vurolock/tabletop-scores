bd460bb3ea68173ea5c1e446a5ed4be531b2cfcd
Seth <s.zim517@gmail.com>
2018-01-18 16:32:35 -0500

    Creates handlebar layout for listing items from db; changes game column from 'title' to 'name'

--- models/game.js
+++ models/game.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = require('../db');
 
 const Game = sequelize.define('game', {
-    title: {
+    name: {
         type: Sequelize.STRING
     }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const Session = require('../models/session');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.route('/')
+  .get((req, res, next) => {
+  res.render('index', { title: 'Tabletop Scores' });
 });
+// Setting up routes
+router.route('/session')
+  .get((req, res) => {
+    res.render('session-list', {
+      listItems: Session
+    });
+  })
+
+
 
 module.exports = router;
