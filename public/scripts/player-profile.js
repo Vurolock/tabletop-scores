124212c3793938b2a73f05df7b56d9915f5cd432
Seth <s.zim517@gmail.com>
2018-01-24 18:18:40 -0500

    Fixes player level function on player-profile.js

--- public/scripts/player-profile.js
+++ public/scripts/player-profile.js
@@ -38,20 +38,21 @@ let filterData = theData => {
 
 let makePlayerLevel = (theData) => {
     let filteredData = theData.filter(obj => homePageId == obj.player.id || userId == obj.player.id);
+    console.log(filteredData.length)
     let filterLength = filteredData.length;
     if (filterLength <= 0) {
         $playerLevel.text(`Level 0 Pissboy`);
-    } else if (0 < filterLength < 5) {
+    } else if (0 < filterLength && filterLength <= 5) {
         $playerLevel.text(`Level ${filterLength} Jester`);
-    } else if (5 < filterLength < 10) {
+    } else if (5 < filterLength && filterLength <= 10) {
         $playerLevel.text(`Level ${filterLength} Squire`);
-    } else if (10 < filterLength < 20) {
+    } else if (10 < filterLength && filterLength <= 20) {
         $playerLevel.text(`Level ${filterLength} Noble`);
-    } else if (20 < filterLength < 30) {
+    } else if (20 < filterLength && filterLength <= 30) {
         $playerLevel.text(`Level ${filterLength} Knight`);
-    } else if (30 < filterLength < 40) {
+    } else if (30 < filterLength && filterLength <= 40) {
         $playerLevel.text(`Level ${filterLength} Master`);
-    } else if (40 < filterLength < 50) {
+    } else if (40 < filterLength && filterLength < 50) {
         $playerLevel.text(`Level ${filterLength} Deity`);
     } else if (filterLength >= 50) {
         $playerLevel.text(`Level ${filterLength} Badass`);
